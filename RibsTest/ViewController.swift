e496ad73997edaa38327d021da215acbe861bbb7
J0onYEong <cjy2134@naver.com>
2024-12-18 17:35:29 +0900

    Initial Commit

---
+++ RibsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RibsTest
+//
+//  Created by choijunios on 12/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
